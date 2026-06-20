331950910fc16b83acf091ce18f95e16ef468441
AumOzaa <programminglaptop1@gmail.com>
2026-06-20 15:30:22 +0530

    Task Creation

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import logger from "./logger.js";
-import { userSignup } from "./validators/validators.js";
+import { taskCreation, userSignup } from "./validators/validators.js";
 import pool from "./neonDemo.js";
 import z, { json } from 'zod';
 import bcrypt from 'bcrypt';
@@ -139,6 +139,8 @@ app.post("/api/user/signin", async (req, res) => {
                 if (isPasswordCorrect) {
 
                     const accessToken = jwt.sign({ "userID": user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+                    logger.info("User signed in");
                     res.status(200).json({
                         "username": user.username,
                         "accessToken": accessToken
@@ -162,6 +164,48 @@ app.post("/api/user/signin", async (req, res) => {
 
         return res.status(500).json({
             message: error.message
+        });
+    }
+});
+
+app.post("/api/user/createtask", async (req, res) => {
+    logger.info("POST /api/user/createtask");
+
+    // ask for jwt token.
+    try {
+        // taking the JWT
+        logger.info("Parsing the token");
+        const token = req.headers['authorization'].split(' ')[1];
+        logger.info("JWT Token parsed");
+
+        // Decoding the payload
+        const decoded_payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        logger.info("Payload decoded Successfuly " + JSON.stringify(decoded_payload));
+
+        // Now creating the task
+        const validateData = taskCreation.parse(req.body);
+        logger.info("User Data Validated Successfully", {
+            "taksName": validateData.taskName
+        });
+
+        logger.info("Querying the dataabse for task creation");
+        const response = await pool.query("INSERT INTO tasks (user_id,name) VALUES ($1,$2) RETURNING *", [decoded_payload.userID, validateData.taskName]);
+
+        logger.info("Task Created Successfully");
+
+        return res.json({
+            "task": validateData.taksName,
+            "message": "Task created succesfully"
+        });
+
+    } catch (error) {
+        logger.error("Something went wrong", {
+            "error": error.errors
+        });
+
+        res.json({
+            "msg": "Something was wrong"
         });
     }
 });
--- validators/validators.js
+++ validators/validators.js
@@ -4,3 +4,7 @@ export const userSignup = z.object({
     username: z.string().min(3),
     password: z.string().min(3)
 });
+
+export const taskCreation = z.object({
+    taskName: z.string().min(2)
+})
